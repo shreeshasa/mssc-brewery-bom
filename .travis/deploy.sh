2f47ad5a691f8d010a08d4b953844b9019a6e7ee
Shreesha Adiga <ShreeshaShankar.Adiga@synchronoss.com>
2020-06-21 10:55:17 +0530

    Initial Commit

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -33,5 +33,7 @@ fi
 # Prepare the local keyring (requires travis to have decrypted the file beforehand)
 gpg --fast-import .travis/gpg-private-key.asc
 
+echo "Branch is ${BRANCH_NAME}"
+
 # Run the maven deploy steps
 ./mvnw deploy -Pdeploy -DskipTests=true "${TRAVIS_BUILD_DIR}/.travis/mvn-settings.xml"
